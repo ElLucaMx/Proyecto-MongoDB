5b7a976eb8d97aacbafa04b6549f99823cce11fe
ElLucaMx <alejandrotejadaperez37@gmail.com>
2025-05-18 20:37:45 +0200

    Delete proyecto directory
    He reestructurado el código para una mejor compresión de este

--- proyecto/db.py
+++
@@ -1,7 +0,0 @@
-# db.py
-from pymongo import MongoClient
-
-def get_collection(uri: str = "mongodb://localhost:27017/", db_name: str = "consolas", coll_name: str = "plataformas"):
-    client = MongoClient(uri)
-    db = client[db_name]
-    return db[coll_name]
--- proyecto/main.py
+++
@@ -1,6 +0,0 @@
-# main.py
-
-from menu import run_menu
-
-if __name__ == "__main__":
-    run_menu()
